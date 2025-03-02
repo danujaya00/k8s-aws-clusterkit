41f08a5e9b28872c051eba7dff6520f611663aaa
danujaya00 <danujaya@wso2.com>
2025-03-02 22:03:46 +0530

    Add VPC and subnet configurations for EC2 cluster setup

---
+++ vpc.tf
@@ -0,0 +1,109 @@
+# Create VPC
+resource "aws_vpc" "ec2_cluster_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name                                = "ec2-cluster"
+    "kubernetes.io/cluster/ec2-cluster" = "shared"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.ec2_cluster_vpc.id
+  cidr_block              = "10.0.16.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                = "ec2-cluster-public-1a"
+    "kubernetes.io/cluster/ec2-cluster" = "owned"
+    "kubernetes.io/role/elb"            = "1"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.ec2_cluster_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name                                = "ec2-cluster-private-1a"
+    "kubernetes.io/cluster/ec2-cluster" = "owned"
+    "kubernetes.io/role/internal-elb"   = "1"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.ec2_cluster_vpc.id
+
+  tags = {
+    Name = "ec2-cluster-igw"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.ec2_cluster_vpc.id
+
+  tags = {
+    Name = "ec2-cluster-public"
+  }
+}
+
+# Route for Public Route Table
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.ec2_cluster_vpc.id
+
+  tags = {
+    Name = "ec2-cluster-private"
+  }
+}
+
+# Allocate Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+# Create NAT Gateway in Public Subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "ec2-cluster-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Route for Private Subnet via NAT Gateway
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
